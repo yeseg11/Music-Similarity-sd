cadbf944d905eba885499bf9ca4775b223577beb
Liranalb <liran.alb@gmail.com>
2021-04-26 13:36:49 +0300

    add methods for getting the user, userdata, and playlists

--- routes/post/loginUser.js
+++ routes/post/loginUser.js
@@ -2,207 +2,183 @@ let PublicUsers = require('../../models/publicUsers.js');
 let UserData = require('../../models/userData.js');
 let PlayList = require('../../models/playlist.js');
 
-let FULLPLAYLISTSONGS = 3;
-let GENERPLAYLISTSONGS =2;
-const TESTFLAG= true
+//add test flag and test the first session
+//take the names from the Word document
+let FULLPLAYLISTSONGS = 3; //need to edit to support the new config file
+let GENREPLAYLISTSONGS = 2;//	"				"				"
+const TESTFIRSTSESSION = true;
+
+module.exports = async function (req, res, next) {  
+		if (!req || !req.body) return res.sendStatus(400); //if the request is empty, return it
+		if (req.body.userName === undefined || req.body.userPassword === undefined)
+			return next(new Error('User name or Password is missing'));
+	
+	try{
+		const user = await getUser(req); // getting the user from PublicUsers collection
+		const userData = await getUserData(user); //getting the user data from UserDatas collection
+		const firstLogin = sessionChecker(userData); //return true for first session, reject if exceeded session limit
+		const mapPlaylistData = await getPlaylistsNames(userData);
+
+		let session;
+		if(firstLogin || TESTFIRSTSESSION) {
+			session = await startFirstSession(mapPlaylistData);
+
+		}
+		
+		else{
+				session = await startSession(mapPlaylistData);
+				const logAnEntrance = await logEntrance(user); // if everything went well, log an entrance
+		}
+		const logAnEntrance = await logEntrance(user); // if everything went well, log an entrance
+		return session;
+	}
+	
+	catch(err){
+		next(err);
+	}
+}
 
 
-module.exports = function (req, res, next) {    //call to getUserData.js , and request all the relevant data from DB
-    if (!req || !req.body) return res.sendStatus(400);
+//grab the user from PublicUsers collection
+function getUser(req){
+	return new Promise(function(resolve,reject) {
+		PublicUsers.find({
+			userName: req.body.userName.toString(),
+			password: req.body.userPassword.toString()
+		}).exec(function (err, docs) {
+        
+		if (err || !docs.length) {
+			reject(new Error('Invalid user!'));
+		}
+		
+		else resolve(docs[0].toObject());
+		})
+	})
+}
 
-    if (req.body.userName === undefined || req.body.userPassword === undefined) {
-        return next(new Error('User name or Password is missing'));
-    }
-
-    PublicUsers.find({
-        userName: req.body.userName.toString(),
-        password: req.body.userPassword.toString()
-    }).exec(function (err, docs) {
-        if (err || !docs.length) return next(err || new Error('Invalid user!'));
-
-        const user = docs[0].toObject();
-
-        // return res.status(200).json({err: false, items: [].concat([user])});
-
+//greab  the user data from UserDatas collection
+function getUserData(user){
+	return new Promise(function(resolve,reject) {
         UserData.find({tamaringaId: user.tamaringaId.toString()})
             .exec(function (err, userDataDocs) {
-                if(err || !userDataDocs.length) return next(err || new Error('Contact support no userData avialable!'));
-                if(userDataDocs[0].researchList === null) return next(err || new Error('Can not Enter, Please add the user to research!'));
-                user.data = userDataDocs[0].toObject();
+                if(err || !userDataDocs.length) 
+					reject(new Error('Error: No userData available!'));
+                else if(userDataDocs[0].researchList === null) reject(new Error('Can not Enter, Please add the user to research!'));
+				else{
+				user.data = userDataDocs[0].toObject();
+				resolve(user.data);
+				}
+		})
+	})
+}
 
-
-
-                user.data.firstTime = user.data.researchList.filter(x=>x.sessionList == null ? 0:x.sessionList).length !== 0?false:true;
-                // @TODO remove me because I'm debug first time
-                //  user.data.researchList[0].sessionList = [];
-
-                if (user.data.researchList[0].sessionList != null){
-                    if(!TESTFLAG && user.data.researchList[0].maxSessionNum === (user.data.researchList[0].sessionList.length )){
-                        return next(new Error('You max the amount of sessions classes!'));
-                    }
-                }
-
-                const logEntrence = () => new Promise((res, rej)=>{
-                    PublicUsers.findOneAndUpdate({_id: user._id}, {$inc: {'entrance': 1}}).exec((err)=>{
-                        if(err) return rej(err);
-
-                        // cla + yid (3 digits code) - get 2 songs
-                        // not 3 digits code - get 3 songs
-                        const mapPlaylistData = user.data.playlists.map(x=>{
-                            return {
-                                name: x,
-                                songs: (x.length == 3) ? GENERPLAYLISTSONGS : FULLPLAYLISTSONGS
-                            }
-                        });
-
-                        const getRandom = (items)=>{
-                            return items[Math.floor(Math.random()*items.length)]
-                        }
-                        if(TESTFLAG || user.data.firstTime){
-
-                            return Promise.all(mapPlaylistData.map(playlistData=>{
-                                return new Promise((pres, prej)=>{
-                                    PlayList.find({name: playlistData.name}).exec((err, playlistsDocs)=>{
-                                        if(err) return prej(err)
-                                        pres(playlistsDocs.map(doc=>{
-                                            const limitSongs = mapPlaylistData.find(x=>x.name==doc.name).songs;
-                                            let records = [];
-                                            while(records.length < limitSongs){
-                                                const record = getRandom(doc.records);
-                                                if(!records.filter(x=>
-                                                    record.mbId.toString()
-                                                    ==
-                                                    x.mbId.toString()
-                                                ).length) records.push(record);
-                                            }
-                                            doc.records = records;
-                                            return doc;
-                                        }));
-                                    })
-                                })
-                            })).then(playlists=>{
-                                //console.log("playlists1: ",playlists)
-                                res(playlists);
-                            }).catch(e=>rej(e))
-                        }else{
-                            //  Not first time user login
-                            const mbidLiked = user.data.researchList[0].sessionList.map(x=>{
-                                return x.songs
-                                    .filter(s=>s.score >= 3 && s.score <= 5)
-                                    .map(s=> s.mbId)
-                            }).reduce(function(o, v, i, arr){
-                                for(let i=0; i < v.length; i++){
-                                    if(o.indexOf(v[i]) != -1) continue;
-                                    o.push(v[i]);
-                                }
-                                return o;
-                            }, []);
-
-
-                            const likedPlaylist = {
-                                "similarity": [],
-                                "_id": null,
-                                "name": "Liked",
-                                "country": null,
-                                "language": "eng",
-                                records: []
-                            }
-
-                            // console.log(likedPlaylist);
-
-                            return Promise.all(mapPlaylistData.map(playlistData=>{
-                                return new Promise((pres, prej)=>{
-                                    PlayList.find({name: playlistData.name}).exec((err, playlistsDocs)=>{
-                                        if(err) return prej(err);
-                                        // join all sessions seen mbid
-                                        // @return mbid[]
-                                        // console.log("playlistsDocs",playlistsDocs);
-                                        for(let i=0; i < playlistsDocs.length;i++){
-                                            const playlist = playlistsDocs[i];
-                                            if(!playlist || !playlist.records || !playlist.records.length) continue;
-                                            for(let r=0;r < playlist.records.length;r++){
-                                                const record = playlist.records[r];
-                                                if(!record) continue;
-                                                if(mbidLiked.indexOf(record.mbId) != -1 && !likedPlaylist.records.find(x=>x.mbId==record.mbId)) likedPlaylist.records.push(record);
-                                            }
-                                        }
-                                        // console.log("likedPlaylist",likedPlaylist);
-
-                                            let songs = playlistsDocs.map(doc=>{
-                                                let records = []; //.concat(doc.records.find(x=>mbidLiked.indexOf(x.mbId)!=-1) || []).slice(0, 2);
-                                                // return the number of songs for each playlist
-                                                const limitSongs = ((limit,defaultlimitSongs)=>{
-                                                    return defaultlimitSongs > limit ? defaultlimitSongs : limit;
-                                                })(mapPlaylistData.find(x=>x.name==doc.name).songs - records.length >= 3 ? 3 : records.length, mapPlaylistData.find(x=>x.name==doc.name).songs);
-
-
-                                                while(records.length < limitSongs){
-                                                    const record = getRandom(doc.records);
-                                                    if(!records.filter(x=>
-                                                        record.mbId.toString()
-                                                        ==
-                                                        x.mbId.toString()
-                                                    ).length) {
-                                                        // prevent duplicate mbid to show on end user
-                                                        if(mbidLiked.indexOf(record.mbId) == -1)
-                                                        records.push(record);
-                                                    }
-                                                }
-                                                doc.records = records;
-                                                return doc;
-                                        });
-                                        pres(songs);
-                                    })
-                                })
-                            })).then((playlists)=>{
-                                // console.log('before res', likedPlaylist);
-                                res([playlists, likedPlaylist]);
-                            }).catch(e=>rej(e))
-
-                        }
-
-                    })
-                });
-
-                logEntrence().then((data)=>{
-                    let likedPL, playlists;
-                    //
-                    if(Array.isArray(data) && data.length == 1 && Array.isArray(data[0])){ //Not the first time enter
-                        [playlists, likedPL] = data;
-                    }else{
-                        playlists = data;
-                    }
-
-                    //console.log("playlists2:",playlists);
-                    if (playlists.length > 1){
-                        user.playlists = playlists.filter(x=>x.length).sort((a,b)=>((b || [])[0].records || []).length - ((a || [])[0].records || []).length);
-                    }
-                    else{
-                        user.playlists = [playlists]
-                    }
-                    //console.log("user.playlists:",user.playlists);
-                    if(typeof likedPL == 'object') user.playlists.unshift([likedPL]);
-                    if(!user.data.researchList.length) return next(new Error('No research list exists!'));
-
-                    let update = {};
-                    update['$push'] = {
-                        'researchList.0.sessionList': {
-                            sessionNumber: (!user.data.researchList[0].sessionList.length) ? 1 : user.data.researchList[0].sessionList.length + 1,
-                            sessionDate: new Date(),
-                            songs: []
-                        }
-                    }
+// Check if it's the first session and if the user has exceded the max number of sessions
+function sessionChecker(userData){
+	//add a research check(if the research is null)
+	userData.firstTime = userData.researchList.filter(x=>x.sessionList == null ? 0:x.sessionList).length !== 0?false:true;
+		if (userData.researchList[0].sessionList != null){
+			if(userData.researchList[0].maxSessionNum === (userData.researchList[0].sessionList.length )){
+				return new Error('Error: User exceeded the max number of sessions!');
+			}
+		}
+		return userData.firstTime;
+}
 
 
 
 
-                    UserData.findOneAndUpdate({_id:  user.data._id}, update).exec((err, result)=>{
-                        if(err) return next(err);
-                        res.status(200).json({err: false, items: [user]})
-                    })
-                }).catch(e=>next(e))
-            });
+//define an object for each playlist with the name and number of songs
+function getPlaylistsNames(user){
+		let playlistData = user.playlists.firstLanguage.playlists;
+		if(user.playlists.secondLanguage.language !== null || user.playlists.secondLanguage.language !== "empty") {
+			playlistData = playlistData.concat(user.playlists.secondLanguage.playlists);
+		}
+
+		if(user.playlists.genrePlaylists !== null) {
+			playlistData = playlistData.concat(user.playlists.genrePlaylists);
+		}
 
 
-    });
-};
+	return playlistData.map(x => {
+		return {
+			name: x,
+			songs: (x.length === 3) ? GENREPLAYLISTSONGS : FULLPLAYLISTSONGS
+		}
+	});
+}
+
+
+
+// at the first session, returning an array with random songs from all of the user's playlists
+async function startFirstSession(mapPlaylistData) {
+try{
+	let records =[];
+	for (const plName of mapPlaylistData) {
+		await PlayList.find({name: playlistData.name}).exec((err, playlistsDocs)=>{
+		if(err)
+			return err;
+		
+		const songLimit = mapPlaylistData.find(x => x.name === plName.name).songs;
+		while(records.length < songLimit){
+			const record = playlistsDocs.records[Math.floor(Math.random() * playlistsDocs.records.length)];
+				if(!records.filter(x=> record.mbId.toString() === x.mbId.toString()).length)
+				records.push(record);
+		}
+		return records;
+
+		//playlistDocs === corrent playlist
+
+		})
+	}
+}
+
+catch(err){
+	return err;
+	}
+}
+	
+
+
+//after the first session, this function will be used
+function startSession(mapPlaylistData) {
+	
+}
+
+function updateUserData(mapPlaylistData) {
+	
+}
+	
+// if everything went well, log an entrace
+function logEntrance(user){
+	return new Promise(function(resolve,reject) {
+		PublicUsers.findOneAndUpdate({_id: user._id}, {$inc: {'entrance': 1}})
+		.exec(function (err, entranceData) {
+			if(err || !entranceData.length) 
+				reject(new Error('Problem register entrance'));
+			else if(entranceData === null) reject(new Error('can\'t register enterace'));
+			else{
+				resolve(entranceData);
+			}
+		})	
+	})
+}
+	
+	
+	//require playlist object by playlist names
+	
+	
+	/* 	
+            User.findOne({"username":req.body.username}, function(err,user) {
+                if (err) {
+                    reject(err)
+                } else {
+                    console.log("yaha b agyaaa");
+                    var errorsArr = [];
+                    errorsArr.push({"msg":"Username already been taken."});
+                    resolve(errorsArr);
+                } */
+
+
+
+
+
