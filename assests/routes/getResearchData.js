d26bc5dd0f4ea9202b722deaab3227ee102d7706
Liranalb <liran.alb@gmail.com>
2021-06-26 15:12:17 +0300

    initiate researcher dashboard support file

---
+++ assests/routes/getResearchData.js
@@ -0,0 +1,7 @@
+(function ($) {
+    $(document).ready(function () {
+        var sessionMenuItem = '<div class="rsStyle-bar-block" id=\'sessionMenuItem\'>';
+        sessionMenuItem += '<a href="#" className="rsStyle-bar-item rsStyle-button rsStyle-padding rsStyle-grad-menu"><i className="fa fa-bullseye fa-fw"></i>::SESSIONMENUITEM::</a>';
+        sessionMenuItem += '</div>';
+    });
+})(jQuery);
