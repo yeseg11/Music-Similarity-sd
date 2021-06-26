80e83b715b543c7c36d3b9c96679c8e948cd5f3e
Liranalb <liran.alb@gmail.com>
2021-06-26 16:12:24 +0300

    add method for getting the research and storing the research data

--- assests/routes/getResearchData.js
+++ assests/routes/getResearchData.js
@@ -1,7 +1,51 @@
 (function ($) {
     $(document).ready(function () {
+        let researchId = $('#researchId').val;
+
+        function initResearchData() {
+            getResearchData().then(function (researchData) {
+                const researchName = researchData[0].researchName;
+                const researchId = researchData[0].researchId;
+                const researchGroup = researchData[0].researchGroupId;
+                const nursingHome = researchData[0].nursingHome;
+                const department = researchData[0].department;
+                const description = researchData[0].description;
+                const patientsIds = researchData[0].patientsIds;
+                const numberOfWeek = researchData[0].numberOfWeeks;
+                const meetingPerWeeks = researchData[0].meetingPerWeek;
+                const lengthOfSession = researchData[0].lengthOfSession;
+            }).catch(function (err) {
+                console.log(err);
+                return err;
+            });
+        }
+
+        function getResearchData() {
+            return new Promise(function (resolve, reject) {
+                $.get('/research/' + researchId, function (data) {
+                    if (!data || !data.items || !data.items.length) return reject(Error("ERROR IN FIND LIST"));
+                    resolve(data.items);
+                })
+            });
+        }
         var sessionMenuItem = '<div class="rsStyle-bar-block" id=\'sessionMenuItem\'>';
         sessionMenuItem += '<a href="#" className="rsStyle-bar-item rsStyle-button rsStyle-padding rsStyle-grad-menu"><i className="fa fa-bullseye fa-fw"></i>::SESSIONMENUITEM::</a>';
         sessionMenuItem += '</div>';
     });
 })(jQuery);
+
+
+// add async supoprt for ajax calls
+function ajaxAwait(resourceUrl, method, playlistData){
+    return  new Promise(function (resolve, reject) {
+        $[method](resourceUrl ,{ playlistData,
+        }).done(function (data) {
+            if (data.err){
+                reject(data);
+            }
+            else {
+                resolve(data);
+            }
+        });
+    });
+}
