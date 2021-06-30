003d52a8c04e08e3c2656eb1b14484f22e1b8115
Liranalb <liran.alb@gmail.com>
2021-06-30 15:44:55 +0300

    add genre playlist count

--- assests/routes/getResearchDash.js
+++ assests/routes/getResearchDash.js
@@ -18,6 +18,10 @@
                 let numberOfSongs = 0;
                 let NumberOfRatedSongs = 0;
 
+
+
+
+
                 let users = $.post('/usersData', {patientsIds}, async function (usersData){
                     usersData = usersData.items;
 
@@ -40,8 +44,58 @@
                     let playlistsData = [];
                     let languageData = [];
 
+                    // let genreData = [
+                    //     {
+                    //         playlistName: "Classical/Traditional",
+                    //         value: "cla",
+                    //         sumOfRaters: 0,
+                    //         sumScore: 0,
+                    //         average: 0,
+                    //     },
+                    //     {
+                    //         playlistName: "Yiddish",
+                    //         value: "yid",
+                    //         sumOfRaters: 0,
+                    //         sumScore: 0,
+                    //         average: 0,
+                    //     },
+                    //     {
+                    //         playlistName: "Arabic",
+                    //         value: "ara",
+                    //         sumOfRaters: 0,
+                    //         sumScore: 0,
+                    //         average: 0,
+                    //     },
+                    //     {
+                    //         playlistName: "Ladino",
+                    //         value: "lad",
+                    //         sumOfRaters: 0,
+                    //         sumScore: 0,
+                    //         average: 0,
+                    //     },
+                    //     {
+                    //         playlistName: "Prayer Songs (Piyutim)",
+                    //         value: "pra",
+                    //         sumOfRaters: 0,
+                    //         sumScore: 0,
+                    //         average: 0,
+                    //     },
+                    //     {
+                    //         playlistName: "Middle Eastern music",
+                    //         value: "mid",
+                    //         sumOfRaters: 0,
+                    //         sumScore: 0,
+                    //         average: 0,
+                    //     },
+                    // ];
+
+
+                    let genreData = [];
+
+
                     // Create an array with the research songs, songs occurrences and average rating
                     allUsersSongs.forEach(function (value) {
+                        let isGenre = false;
                         if(!songStatistics[value.mbId]) {
                             songStatistics[value.mbId] = { //add songs to songStatistics if he isn't there already
                                 sumScore: 0,
@@ -53,7 +107,26 @@
                             }
                             numberOfSongs++;
                         }
-                        if(!playlistsData[value.playlistName]){
+
+                        if(value.playlistName.localeCompare("cla") === 0
+                            || value.playlistName.localeCompare("yid") === 0
+                            || value.playlistName.localeCompare("lad") === 0
+                            || value.playlistName.localeCompare("pra") === 0
+                            || value.playlistName.localeCompare("mid") === 0){
+                            isGenre = true;
+                        }
+
+                        if(!playlistsData[value.playlistName] && isGenre){
+                            genreData[value.playlistName] = {
+                                playlistName: value.playlistName,
+                                sumOfRaters: 0,
+                                sumScore: 0,
+                                average: 0,
+                            }
+                            genreData.length++;
+                        }
+
+                        if(!playlistsData[value.playlistName] && !isGenre){
                             playlistsData[value.playlistName] = {
                                 playlistName: value.playlistName,
                                 sumOfRaters: 0,
@@ -69,9 +142,17 @@
                         songStatistics[value.mbId].language = value.language;
                         songStatistics[value.mbId].playlistName = value.playlistName;
                         if(value.score > 0){
-                            playlistsData[value.playlistName].sumScore += value.score;
-                            playlistsData[value.playlistName].sumOfRaters++;
-                            playlistsData[value.playlistName].average = playlistsData[value.playlistName].sumScore / playlistsData[value.playlistName].sumOfRaters;
+                            if(!isGenre) {
+                                playlistsData[value.playlistName].sumScore += value.score;
+                                playlistsData[value.playlistName].sumOfRaters++;
+                                playlistsData[value.playlistName].average = playlistsData[value.playlistName].sumScore / playlistsData[value.playlistName].sumOfRaters;
+                            }
+
+                            if(isGenre) {
+                                genreData[value.playlistName].sumScore += value.score;
+                                genreData[value.playlistName].sumOfRaters++;
+                                genreData[value.playlistName].average = genreData[value.playlistName].sumScore / genreData[value.playlistName].sumOfRaters;
+                            }
 
                             songStatistics[value.mbId].sumOfRaters++;
                             songStatistics[value.mbId].average = songStatistics[value.mbId].sumScore / songStatistics[value.mbId].sumOfRaters;
